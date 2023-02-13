35d90f612287a1efd885acfd441fb4d8c92bc56d
arhama haleem <arhama.haleem1@outlook.com>
2023-02-13 05:27:36 +0000

    Updated index.php

--- index.php
+++ index.php
@@ -10,5 +10,10 @@ catch (PDOException $e) {
     die(print_r($e));
 }
 
-
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "serveradmin2", "pwd" => "zxcvbnm1!", "Database" => "floteq_dev", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:testdbsqlserver2.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
+
+
