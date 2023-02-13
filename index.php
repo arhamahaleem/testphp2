1d5d9c67f760e061c6da1146c9694ec99e5841fb
arhama haleem <arhama.haleem1@outlook.com>
2023-02-13 05:56:26 +0000

    Added index.php

---
+++ index.php
@@ -0,0 +1,17 @@
+<?php
+// PHP Data Objects(PDO) Sample Code:
+try {
+    $conn = new PDO("sqlsrv:server = tcp:testdbsqlserver2.database.windows.net,1433; Database = floteq_dev", "serveradmin2", "zxcvbnm1!");
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     print("connected");
+}
+catch (PDOException $e) {
+    print("Error connecting to SQL Server.");
+    die(print_r($e));
+}
+
+// SQL Server Extension Sample Code:
+$connectionInfo = array("UID" => "serveradmin2", "pwd" => "zxcvbnm1!", "Database" => "floteq_dev", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$serverName = "tcp:testdbsqlserver2.database.windows.net,1433";
+$conn = sqlsrv_connect($serverName, $connectionInfo);
+?>
