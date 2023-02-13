e7771cedc423b2bb80fa70467d3020e15303a431
arhama haleem <arhama.haleem1@outlook.com>
2023-02-13 09:59:42 +0000

    Updated index.php

--- index.php
+++ index.php
@@ -4,7 +4,11 @@
 try {
     $conn = new PDO("sqlsrv:server = tcp:testdbsqlserver2.database.windows.net,1433; Database = floteq_dev", "serveradmin2", "zxcvbnm1!");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    print("connected");
+    $sql =("SELECT * from PaymentGateWay where PaymentGateWayID=2");
+    $stmt = $conn->query("$sql");
+    $row = $stmt->fetch();
+    echo"$row[0] $row[1] $row[2]";
+    $conn = NULL;
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
