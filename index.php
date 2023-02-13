f8a49aabe7884ab57d47ca1bc1bc8c7fdce8620e
arhama haleem <arhama.haleem1@outlook.com>
2023-02-13 05:18:25 +0000

    Updated index.php

--- index.php
+++ index.php
@@ -1,10 +1,9 @@
 <?php
 // PHP Data Objects(PDO) Sample Code:
 try {
-    $conn = new PDO("sqlsrv:server = tcp:testdbsqlserver2.database.windows.net,1433; Database = newdatabase", "serveradmin2", "zxcvbnm1!");
+    $conn = new PDO("sqlsrv:server = tcp:testdbsqlserver2.database.windows.net,1433; Database = floteq_dev", "serveradmin2", "zxcvbnm1!");
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    
-    print("connect to SQL Server.");
+    print("connect to sql server");
 }
 catch (PDOException $e) {
     print("Error connecting to SQL Server.");
@@ -12,7 +11,7 @@ catch (PDOException $e) {
 }
 
 // SQL Server Extension Sample Code:
-$connectionInfo = array("UID" => "serveradmin2", "pwd" => "zcvbnm1!", "Database" => "newdatabase", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+$connectionInfo = array("UID" => "serveradmin2", "pwd" => "zxcvbnm1!", "Database" => "floteq_dev", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
 $serverName = "tcp:testdbsqlserver2.database.windows.net,1433";
 $conn = sqlsrv_connect($serverName, $connectionInfo);
 ?>
