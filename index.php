5c78f4f7a01bf744b1d9d5ccdd0eef80104220c5
arhama haleem <arhama.haleem1@outlook.com>
2023-02-13 05:35:41 +0000

    Updated index.php

--- index.php
+++ index.php
@@ -2,15 +2,15 @@
 // PHP Data Objects(PDO) Sample Code:
 
     print("connection string");
-try {
-    $conn = new PDO("sqlsrv:server = tcp:testdbsqlserver2.database.windows.net,1433; Database = floteq_dev", "serveradmin2", "zxcvbnm1!");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    print("connect to sql server");
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
+// try {
+//     $conn = new PDO("sqlsrv:server = tcp:testdbsqlserver2.database.windows.net,1433; Database = floteq_dev", "serveradmin2", "zxcvbnm1!");
+//     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+//     print("connect to sql server");
+// }
+// catch (PDOException $e) {
+//     print("Error connecting to SQL Server.");
+//     die(print_r($e));
+// }
 ?>
 
 
