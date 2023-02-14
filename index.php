e56a0e606ab2bc4302e4daba4e5f01a3e413869e
arhamahaleem <125270686+arhamahaleem@users.noreply.github.com>
2023-02-14 09:57:22 +0500

    row 0-4

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ try {
     $sql =("SELECT * from PaymentGateWay where PaymentGateWayID=2");
     $stmt = $conn->query($sql);
     $row = $stmt->fetch();
-    echo"$row[0] $row[1] $row[2] $row[3] $row[4] $row[5] $row[6]";
+    echo"$row[0] $row[1] $row[2] $row[3] $row[4]";
     $conn = NULL;
 }
 catch (PDOException $e) {
